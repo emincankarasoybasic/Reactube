31bc9492367d874f9bf506665872239407a1d50e
Emincan Karasoy <emincannkarasoy@gmail.com>
2022-09-20 22:14:33 +0300

    Fix root render problem

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM  from 'react-dom';
+import ReactDOM  from 'react-dom/client';
 import App from'./components/App';
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
-ReactDOM.render(<App/>,root);
+root.render(<App/>);
